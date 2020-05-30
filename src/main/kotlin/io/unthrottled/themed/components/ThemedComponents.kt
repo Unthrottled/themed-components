e12f458fb7b4c8a42ce20996f36c14ac2c16b02b
DATBOI <alexsimons9999@gmail.com>
2020-05-30 09:22:39 -0500

    More using the plugin version so my dumbass does not forget to change it in more than one place

--- src/main/kotlin/io/unthrottled/themed/components/ThemedComponents.kt
+++ src/main/kotlin/io/unthrottled/themed/components/ThemedComponents.kt
@@ -1,19 +1,25 @@
 package io.unthrottled.themed.components
 
+import com.intellij.ide.plugins.PluginManagerCore
 import com.intellij.ide.ui.LafManagerListener
 import com.intellij.openapi.Disposable
 import com.intellij.openapi.application.ApplicationManager
+import com.intellij.openapi.extensions.PluginId
 import com.intellij.openapi.project.Project
 import com.intellij.openapi.project.ProjectManager
 import com.intellij.openapi.project.ProjectManagerListener
-import io.unthrottled.themed.components.notification.CURRENT_VERSION
 import io.unthrottled.themed.components.notification.UpdateNotification
 import io.unthrottled.themed.components.laf.LookAndFeelInstaller.installAllUIComponents
 import io.unthrottled.themed.components.settings.Configurations
+import io.unthrottled.themed.components.util.toOptional
+import java.util.*
 
 class ThemedComponents : Disposable {
     private val connection = ApplicationManager.getApplication().messageBus.connect()
 
+    companion object {
+      private const val PLUGIN_ID = "io.unthrottled.themed-components"
+    }
     init {
         installAllUIComponents()
 
@@ -22,16 +28,22 @@ class ThemedComponents : Disposable {
         })
         connection.subscribe(ProjectManager.TOPIC, object : ProjectManagerListener {
             override fun projectOpened(project: Project) {
-                if (Configurations.instance.version != CURRENT_VERSION) {
-                    Configurations.instance.version =
-                        CURRENT_VERSION
-                    UpdateNotification.display(project)
+              getVersion()
+                .filter { it != Configurations.instance.version }
+                .ifPresent {
+                  Configurations.instance.version = it
+                  UpdateNotification.display(project, it)
                 }
             }
         })
     }
 
-    override fun dispose() {
+  private fun getVersion(): Optional<String> =
+    PluginManagerCore.getPlugin(PluginId.getId(PLUGIN_ID)).toOptional()
+      .map { it.version }
+
+
+  override fun dispose() {
         connection.dispose()
     }
 }
