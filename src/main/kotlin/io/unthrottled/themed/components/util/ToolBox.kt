d025b5e8c32e86b75ed4f27b3b3e0c990d51de62
DATBOI <alexsimons9999@gmail.com>
2020-12-05 15:43:42 -0600

    Removing the dialog on sheet windows

--- src/main/kotlin/io/unthrottled/themed/components/util/ToolBox.kt
+++ src/main/kotlin/io/unthrottled/themed/components/util/ToolBox.kt
@@ -2,30 +2,30 @@ package io.unthrottled.themed.components.util
 
 import com.intellij.ui.ColorUtil
 import java.awt.Color
-import java.util.*
+import java.util.Optional
 import java.util.concurrent.Callable
 import java.util.stream.Stream
 
 fun <T> getSafely(callable: Callable<T>): Optional<T> =
-    try {
-      callable.call().toOptional()
-    } catch (e: Throwable) {
-      Optional.empty()
-    }
+  try {
+    callable.call().toOptional()
+  } catch (e: Throwable) {
+    Optional.empty()
+  }
 
-fun runSafely(runner: Runner): Unit =
-    try {
-      runner.run()
-    } catch (e: Throwable) {
-    }
+fun runSafely(
+  runner: () -> Unit,
+  onError: (Throwable) -> Unit = {}
+): Unit =
+  try {
+    runner()
+  } catch (e: Throwable) {
+    onError(e)
+  }
 
 fun <T> T?.toOptional() = Optional.ofNullable(this)
 
 fun <T> T?.toStream(): Stream<T> = Stream.of(this)
-
-interface Runner {
-    fun run()
-}
 
 fun Color.toHexString() = "#${ColorUtil.toHex(this)}"
 
