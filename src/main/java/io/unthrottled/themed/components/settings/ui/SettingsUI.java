a2629d77f8489584ab3bb4bda92eaa726e9a3c37
DATBOI <alexsimons9999@gmail.com>
2021-02-17 18:33:42 -0600

    Working on building out the new settings page

---
+++ src/main/java/io/unthrottled/themed/components/settings/ui/SettingsUI.java
@@ -0,0 +1,16 @@
+package io.unthrottled.themed.components.settings.ui;
+
+import javax.swing.*;
+
+public class SettingsUI {
+  private JTabbedPane tabbedPane1;
+  private JPanel panel1;
+  private JCheckBox themedTitleBarBox;
+  private JPanel foregroundColorPanel;
+  private JPanel inactiveForeground;
+  private JCheckBox enableCustomColors;
+
+  private void createUIComponents() {
+    // TODO: place custom component creation code here
+  }
+}
---
+++ src/main/java/io/unthrottled/themed/components/settings/ui/bruh.java
@@ -0,0 +1,3 @@
+package io.unthrottled.themed.components.settings.ui;
+public class bruh {
+}
