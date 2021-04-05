b115504d82f1e8435a87749c0d5050dcc62deace
DATBOI <alexsimons9999@gmail.com>
2021-04-05 17:42:24 -0500

    Updated sentry client

--- src/main/kotlin/io/unthrottled/themed/components/integrations/RestClient.kt
+++ src/main/kotlin/io/unthrottled/themed/components/integrations/RestClient.kt
@@ -1,44 +1,45 @@
 package io.unthrottled.themed.components.integrations
 
+import com.intellij.openapi.application.ApplicationManager
 import com.intellij.openapi.diagnostic.Logger
+import com.intellij.util.io.HttpRequests
+import io.unthrottled.themed.components.integrations.RestTools.performRequest
 import io.unthrottled.themed.components.util.readAllTheBytes
 import io.unthrottled.themed.components.util.toOptional
-import org.apache.http.client.config.RequestConfig
-import org.apache.http.client.methods.HttpGet
-import org.apache.http.impl.client.HttpClients
+import java.io.InputStream
 import java.util.Optional
-import java.util.concurrent.TimeUnit
+import java.util.concurrent.Callable
 
 object RestClient {
-  private val httpClient = HttpClients.createMinimal()
+
+  fun performGet(url: String): Optional<String> =
+    ApplicationManager.getApplication().executeOnPooledThread(
+      Callable {
+        performRequest(url) { responseBody ->
+          String(responseBody.readAllTheBytes())
+        }
+      }
+    ).get()
+}
+
+object RestTools {
   private val log = Logger.getInstance(this::class.java)
 
-  fun performGet(url: String): Optional<String> {
-    log.info("Attempting to fetch remote asset: $url")
-    val request = createGetRequest(url)
-    return try {
-      val response = httpClient.execute(request)
-      val statusCode = response.statusLine.statusCode
-      log.info("Asset has responded for remote asset: $url with status code $statusCode")
-      if (statusCode == 200) {
-        response.entity.content.use { responseBody ->
-          String(responseBody.readAllTheBytes())
-        }.toOptional()
-      } else {
-        Optional.empty()
+  fun <T> performRequest(
+    url: String,
+    bodyExtractor: (InputStream) -> T
+  ): Optional<T> {
+    log.info("Attempting to download remote asset: $url")
+    return HttpRequests.request(url)
+      .connect { request ->
+        try {
+          val body = bodyExtractor(request.inputStream)
+          log.info("Asset has responded for remote asset: $url")
+          body.toOptional()
+        } catch (e: HttpRequests.HttpStatusException) {
+          log.warn("Unable to get remote asset: $url for raisins", e)
+          Optional.empty<T>()
+        }
       }
-    } catch (e: Throwable) {
-      log.warn("Unable to get remote asset: $url for raisins", e)
-      Optional.empty<String>()
-    }
-  }
-
-  private fun createGetRequest(remoteUrl: String): HttpGet {
-    val remoteAssetRequest = HttpGet(remoteUrl)
-    remoteAssetRequest.addHeader("User-Agent", "Themed Components")
-    remoteAssetRequest.config = RequestConfig.custom()
-      .setConnectTimeout(TimeUnit.MILLISECONDS.convert(5L, TimeUnit.SECONDS).toInt())
-      .build()
-    return remoteAssetRequest
   }
 }
